4676c4101f64eb8274fd35a808714cfe3da06ba5
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-11 20:50:08 +0200

    Zaglavlje promene

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,15 @@ $(document).ready(function () {
 
     //MOBILE ONE
     var menu = "close";
-    $("nav .menu-toggle").on('click touchstart',function () {
+    $("nav .menu-toggle").on('click touch',function () {
 
         if (menu === "close") {
             $(this).parent().next(".mobile-nav").css("transform", "translate(0, 0)");
+            $(this).parent().next(".mobile-nav").css("-webkit-transform", "translate(0, 0)");
             menu = "open";
         } else {
             $(this).parent().next(".mobile-nav").css("transform", "translate(-100%, 0)");
+            $(this).parent().next(".mobile-nav").css("-webkit-transform", "translate(-100%, 0)");
             menu = "close";
         }
     });
