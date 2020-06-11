1822e989106413ce205d5366fdff853880027e90
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-11 22:18:07 +0200

    ch

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,8 @@
 $(document).ready(function () {
 
-    //MOBILE ONE
-    var menu = "close";
-    $(document).on('click touch tap',"nav .menu-toggle",function () {
+    $(document).on('click touch',"nav .menu-toggle",function () {
 
-        if (menu === "close") {
-            $(this).parent().next(".mobile-nav").toggleClass("open");
-            menu = "open";
-        } else {
-            $(this).parent().next(".mobile-nav").toggleClass("open");
-            menu = "close";
-        }
+        $(this).parent().next(".mobile-nav").toggleClass("open");
     });
 
 
