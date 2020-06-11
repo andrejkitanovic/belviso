303732d681547ea47a9692e8cb20d3b76ac80fe4
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-11 21:45:12 +0200

    Resenje

--- js/nav.js
+++ js/nav.js
@@ -5,13 +5,10 @@ $(document).ready(function () {
     $(document).on('click touch tap',"nav .menu-toggle",function () {
 
         if (menu === "close") {
-            $(this).parent().next(".mobile-nav").css("transform", "translate(0, 0)");
-            $(this).parent().next(".mobile-nav").css("-webkit-transform", "translate(0, 0)");
+            $(this).parent().next(".mobile-nav").css({"transform":"translate(0, 0)","-webkit-transform": "translate(0, 0)"});
             menu = "open";
-            document.write("TEST");
         } else {
-            $(this).parent().next(".mobile-nav").css("transform", "translate(-100%, 0)");
-            $(this).parent().next(".mobile-nav").css("-webkit-transform", "translate(-100%, 0)");
+            $(this).parent().next(".mobile-nav").css({"transform": "translate(-100%, 0)","-webkit-transform":"translate(-100%, 0)"});
             menu = "close";
         }
     });
