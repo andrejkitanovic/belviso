5a8c3ef10e5d2527dc2e85cda750aa6f622bbc2b
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-11 21:51:47 +0200

    ch

--- js/nav.js
+++ js/nav.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
     $(document).on('click touch tap',"nav .menu-toggle",function () {
 
         if (menu === "close") {
-            $(this).parent().next(".mobile-nav").css({"transform":"translate(0, 0)","-webkit-transform": "translate(0, 0)"});
+            $(this).parent().next(".mobile-nav").toggleClass("open");
             menu = "open";
         } else {
-            $(this).parent().next(".mobile-nav").css({"transform": "translate(-100%, 0)","-webkit-transform":"translate(-100%, 0)"});
+            $(this).parent().next(".mobile-nav").toggleClass("open");
             menu = "close";
         }
     });
