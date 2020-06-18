c16028dcc8ccc40590848478bbe9de28989dae12
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-18 14:52:34 +0200

    hamburger

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     $(document).on('click touch',"nav .menu-toggle",function () {
         $(this).parent().next(".mobile-nav").toggleClass("open");
+        $("nav .header").toggleClass("open-nav");
     });
 
 });
